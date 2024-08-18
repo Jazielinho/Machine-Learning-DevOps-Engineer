a1e886ba2ecca07e6973ddd1064c2eb0ffb51558
jahaziel <jahaziel@senstile.com>
2024-08-18 11:54:04 +0200

    Machine learning pipelines write end to end machine learning pipeline

--- 03_Building_reproducible_model_workflow/06_final_pipeline_releae_and_deploy/01_write_an_end_to_end_machine_learning_pipeline/solution/main.py
+++ 03_Building_reproducible_model_workflow/06_final_pipeline_releae_and_deploy/01_write_an_end_to_end_machine_learning_pipeline/solution/main.py
@@ -45,9 +45,9 @@ def go(config: DictConfig):
             "main",
             parameters={
                 "input_artifact": "raw_data.parquet:latest",
-                "artifact_name": "preprocessed_data.parquet",
+                "artifact_name": "preprocessed_data.csv",
                 "artifact_type": "preprocessed_data",
-                "artifact_description": "Data after preprocessing"
+                "artifact_description": "Data after preprocessing applied"
             },
         )
 
@@ -58,9 +58,9 @@ def go(config: DictConfig):
             os.path.join(root_path, "check_data"),
             "main",
             parameters={
-                "reference_artifact": "raw_data.parquet:latest",
-                "sample_artifact": "preprocessed_data.parquet:latest",
-                "ks_alpha": "0.05"
+                "reference_artifact": config["data"]["reference_dataset"],
+                "sample_artifact": "preprocessed_data.csv:latest",
+                "ks_alpha": config["data"]["ks_alpha"],
             },
         )
 
@@ -71,10 +71,11 @@ def go(config: DictConfig):
             os.path.join(root_path, "segregate"),
             "main",
             parameters={
-                "input_artifact": "preprocessed_data.parquet:latest",
-                "artifact_name": "train_test_data.parquet",
-                "artifact_type": "train_test_data",
-                "artifact_description": "Data after segregation"
+                "input_artifact": "preprocessed_data.csv:latest",
+                "artifact_root": "data",
+                "artifact_type": "segregated_data",
+                "test_size": config["data"]["test_size"],
+                "stratify": config["data"]["stratify"],
             },
         )
 
@@ -87,12 +88,30 @@ def go(config: DictConfig):
             fp.write(OmegaConf.to_yaml(config["random_forest_pipeline"]))
 
         ## YOUR CODE HERE: call the random_forest step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, "random_forest"),
+            "main",
+            parameters={
+                "train_data": "data_train.csv:latest",
+                "model_config": model_config,
+                "export_artifact": config["random_forest_pipeline"]["export_artifact"],
+                "random_seed": config["main"]["random_seed"],
+                "val_size": config["data"]["test_size"],
+                "stratify": config["data"]["stratify"],
+            },
+        )
 
     if "evaluate" in steps_to_execute:
 
         ## YOUR CODE HERE: call the evaluate step
-        pass
+        _ = mlflow.run(
+            os.path.join(root_path, "evaluate"),
+            "main",
+            parameters={
+                "model_export": config["random_forest_pipeline"]["export_artifact"],
+                "test_data": "data_test.csv:latest",
+            },
+        )
 
 
 if __name__ == "__main__":
